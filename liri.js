8779ab486f2e2fa7cd364c8ab590a01b4dce39dd
Felix Fernandez <fernandezfelix460.gmail.com>
2019-11-14 11:54:33 -0500

    created spotify function

--- liri.js
+++ liri.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 // variables
 
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 var moment = require("moment");
 moment().format();
@@ -29,30 +30,30 @@ switch (userOption) {
 };
 function concertThis(userInput) {
 
-    
+
 
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
-      
+
         function (response) {
             console.log(response.data);
             console.log(userInput)
             for (var i = 0; i < response.data.length; i++) {
                 console.log("========================================================================================");
                 console.log("This artist will be performing at " + response.data[i].venue.name + " on this date " + response.data[i].datetime);
-               
+
             }
         });
 };
 
 
-function movieThis(){
-    if(!userInput) {
+function movieThis() {
+    if (!userInput) {
         userInput === "Mr. Nobody";
     };
     var movieQueryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
-axios.get(movieQueryUrl).then(
-    function (response) {
-        // for (var i = 0; i < response.data.length; i++) {
+    axios.get(movieQueryUrl).then(
+        function (response) {
+            // for (var i = 0; i < response.data.length; i++) {
             console.log("========================================================================================")
             console.log("The " + userInput + " came out in the year: " + response.data.Year);
             console.log("Imdb Rating: " + response.data.imdbRating);
@@ -60,28 +61,42 @@ axios.get(movieQueryUrl).then(
             console.log("Actors: " + response.data.Actors);
             console.log("Language: " + response.data.Language);
             console.log("This movie was produced in: " + response.data.Country);
-        // }
+            // }
 
-    
-    });
 
+        });
+
+};
+
+function spotifySong() {
+
+    if (!userInput) {
+        userInput === "The Sign";
     };
+    spotify.search({ type: "track", query: userInput }).then(function (response) {
+        for (var i = 0; i < 1; i++) {
+            console.log("---------------------------------------------------------")
+            console.log("Name of the song " + response.tracks.items[i].name);
+            console.log("Name of the artist " + response.tracks.items[i].artists[0].name);
+            console.log("Album's name " + response.tracks.items[i].album.name)
+            console.log("Song's Preview on Spotify " + response.tracks.items[i].preview_url);
+        }
+    });
+};
 
 function doThis() {
-var fs = require("fs");
+    var fs = require("fs");
 
-fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
+        if (error) {
+            return console.log(error);
+        }
 
-  if (error) {
-    return console.log(error);
-  }
+        var dataArr = data.split(",");
 
-  var dataArr = data.split(",");
-
-//   spotifySong(dataArr[0], dataArr[1]);
-  console.log(dataArr);
-});
+        console.log(dataArr);
+    });
 };
 
 
